091daf04737824487bd575d4c2313373e8e044a6
jfranciscov <jfranciscov7@gmail.com>
2017-09-15 11:09:42 -0300

    user-events-example

---
+++ events/app.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		users: [
+		{
+			name: 'Juan-Francisco'
+			,age: 27
+			,photo: 'https://avatars3.githubusercontent.com/u/8474443?v=4&s=40'
+			,editable: true
+			,deletable: false
+
+		},
+		{
+			name: 'Dog'
+			,age: 0
+			,photo: 'http://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg'
+			,editable: true
+			,deletable: true
+		}
+		]
+		
+	},
+	methods:{
+		edit: function(){
+			console.log('editing...');
+		},
+		remove: function(){
+			console.log('removing...');
+		},
+		getJSON: function(user){
+			alert(JSON.stringify(user));
+		}
+	}
+});
