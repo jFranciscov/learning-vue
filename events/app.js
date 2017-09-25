831aec687d04ed3cb896e2f3d470114d0cc26c2d
jfranciscov <jfranciscov7@gmail.com>
2017-09-24 23:47:00 -0300

    editing

--- events/app.js
+++ events/app.js
@@ -7,6 +7,7 @@ var app = new Vue({
 			,age: 27
 			,photo: 'https://avatars3.githubusercontent.com/u/8474443?v=4&s=40'
 			,editable: true
+			,editing: false
 			,deletable: false
 
 		},
@@ -15,14 +16,15 @@ var app = new Vue({
 			,age: 0
 			,photo: 'http://zoomiedogs.com/wp-content/uploads/dog-icon1.png'
 			,editable: true
+			,editing: false
 			,deletable: true
 		}
 		]
 		
 	},
 	methods: {
-		edit: function(){
-			console.log('editing...');
+		edit: function(user){
+			user.editing = !user.editing;
 		},
 		remove: function(){
 			console.log('removing...');
