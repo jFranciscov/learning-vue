7954ef53c42999d5a57e746f0e4058b18c5659ba
jfranciscov <jfranciscov7@gmail.com>
2017-09-29 00:35:49 -0300

    folder structure reorganization

--- bind/app.js
+++
@@ -1,8 +0,0 @@
-var app = new Vue({
-	el: '#app',
-	data: {
-		name: 'Juan-Francisco',
-		age: 27,
-		photo: 'https://avatars3.githubusercontent.com/u/8474443?v=4&s=40'
-	}
-})
--- helloworld/app.js
+++
@@ -1,7 +0,0 @@
-var app = new Vue({
-	el: '#app',
-	data: {
-		name: 'Juan-Francisco',
-		age: 27
-	}
-})
