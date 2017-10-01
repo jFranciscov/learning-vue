8660c9fe91a6392db72bc344b629f9fae7ba5019
jfranciscov <jfranciscov7@gmail.com>
2017-10-01 02:40:35 -0300

    node server added, consuming rest from vue

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express'),
+path = require('path');
+
+var app = express();
+
+app.set('port', 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/users', function(req, res){
+	var users = [
+	{
+		id: 0
+		,name: 'Juan-Francisco'
+		,age: 27
+		,photo: 'https://avatars3.githubusercontent.com/u/8474443?v=4&s=40'
+		,position: 'Frontend Developer'
+		,editable: true
+		,editing: false
+		,deletable: false
+		,technologies: [
+		{id: 0, name: 'HTML'}
+		,{id: 1, name: 'CSS'}
+		,{id: 2, name: 'Javascript'}
+		,{id: 3, name: 'Vue.js'}
+		,{id: 4, name: 'Bootstrap'}
+		,{id: 5, name: 'GIT'}
+		,{id: 6, name: 'REST'}
+		]
+
+	},
+	{
+		id: 1
+		,name: 'Dog'
+		,age: 0
+		,photo: 'http://zoomiedogs.com/wp-content/uploads/dog-icon1.png'
+		,position: 'Backend Developer'
+		,editable: true
+		,editing: false
+		,deletable: true
+		,technologies: [
+		{id: 0, name: 'Javascript'}
+		,{id: 0, name: 'Node'}
+		,{id: 0, name: 'Gulp'}
+		,{id: 0, name: 'Express'}
+		,{id: 0, name: 'MariaDB'}
+		,{id: 0, name: 'GIT'}
+		,{id: 0, name: 'REST'}
+		]
+	}
+	];
+	res.send(users);
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Servidor corriendo');
+});
+
+
+
+
--- public/app.js
+++ public/app.js
@@ -3,50 +3,17 @@ Vue.component('technology-item', {
 	template: '<li>{{ technology.name }}</li>'
 });
 
-var app = new Vue({
-	el: '#app',
+var users = new Vue({
+	el: '#users',
 	data: {
-		users: [
-		{
-			id: 0
-			,name: 'Juan-Francisco'
-			,age: 27
-			,photo: 'https://avatars3.githubusercontent.com/u/8474443?v=4&s=40'
-			,position: 'Frontend Developer'
-			,editable: true
-			,editing: false
-			,deletable: false
-			,technologies: [
-			{id: 0, name: 'HTML'}
-			,{id: 1, name: 'CSS'}
-			,{id: 2, name: 'Javascript'}
-			,{id: 3, name: 'Vue.js'}
-			,{id: 4, name: 'Bootstrap'}
-			,{id: 5, name: 'GIT'}
-			,{id: 6, name: 'REST'}
-			]
-
-		},
-		{
-			id: 1
-			,name: 'Dog'
-			,age: 0
-			,photo: 'http://zoomiedogs.com/wp-content/uploads/dog-icon1.png'
-			,position: 'Backend Developer'
-			,editable: true
-			,editing: false
-			,deletable: true
-			,technologies: [
-			{id: 0, name: 'Javascript'}
-			,{id: 0, name: 'Nodejs'}
-			,{id: 0, name: 'Express'}
-			,{id: 0, name: 'MariaDB'}
-			,{id: 0, name: 'Gulp'}
-			,{id: 0, name: 'GIT'}
-			,{id: 0, name: 'REST'}
-			]
-		}
-		]
+		users: []
+	},
+	created: function(){
+		var self = this;
+		axios.get('http://localhost:3000/api/users').
+		then(function(response){
+			self.users = response.data;
+		});
 	},
 	methods: {
 		edit: function(user){
